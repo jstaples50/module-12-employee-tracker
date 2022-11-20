1171a1a1ddfa77766c402ae708dd9731d788c070
Jared Staples <jstaplesmusic@gmail.com>
2022-11-20 12:09:43 -0500

    Set up server.js and created sql files

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
